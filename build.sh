fa39b1b1e34d7eb01f936acf20ddff401edf862a
shawon34 <shawonparvez34@gmail.com>
2025-07-30 17:27:11 +0800

    Downgrade pandas to fix compilation issues

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
+# Install system dependencies
+sudo apt-get update
+sudo apt-get install -y build-essential libssl-dev libffi-dev python3-dev
+
+# Install Python dependencies
 pip install --upgrade pip
 pip install wheel
 pip install -r requirements.txt
